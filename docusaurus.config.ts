a0049b66c214cf5496b0c9306bdd4481b0f10acc
nicolas415 <nicolaslaudinet.dev@gmail.com>
2025-01-07 10:37:12 +0100

    Updated header name

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,7 +58,7 @@ const config: Config = {
         // Replace with your project's social card
         image: "img/docusaurus-social-card.jpg",
         navbar: {
-            title: "Nicolas415",
+            title: "Nicolas Laudinet",
             logo: {
                 alt: "Notes Logo",
                 src: "img/logo.svg",
